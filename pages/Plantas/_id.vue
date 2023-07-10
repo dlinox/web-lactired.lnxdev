fbfa8835e6d4c02e994cf3af272481aff1b793e3
Lino <nearlino20@gmail.com>
2023-07-09 20:56:59 -0500

    first commit

---
+++ pages/Plantas/_id.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <Header />
+      <PageHeader title="Service Detail" />
+      <ServiceDetail />
+      <Footer />
+    </div>
+  </template>
+  <script>
+    import Header from "@/components/Header";
+    import PageHeader from "@/components/PageHeader";
+    import ServiceDetail from "@/components/ServiceDetail";
+    import Footer from "@/components/Footer";
+    export default {
+      components: {
+    Footer,
+    ServiceDetail,
+    PageHeader,
+    Header
+},
+      head(){
+        return {
+          title: "Agrikol  | Service Detail"
+        }
+      }
+    }
+  </script>
+  
---
+++ pages/Plantas/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <Header />
+        <PageHeader title="Plantas" />
+
+
+        <section class="recent-project our_projects">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="recent_project_single mrb-30">
+                            <div class="project_img_box">
+                                <img src="/assets/images/project/recent-pro-img-1.jpg" alt="Recent Project Img">
+                                <div class="project_content">
+                                    <h3>organic<br>solutions</h3>
+                                </div>
+                                <div class="hover_box">
+                                    <nuxt-link to="/plantas/1"><span class="icon-left-arrow"></span></nuxt-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <Footer />
+    </div>
+</template>
+  
+<script>
+
+import Nav from "@/components/Nav";
+import PageHeader from "@/components/PageHeader";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+export default {
+    components: {
+        Footer,
+        PageHeader,
+        Nav,
+        Header
+    },
+    head() {
+        return {
+            title: "Agrikol  | Our Projects"
+        }
+    }
+}
+</script>
+  
